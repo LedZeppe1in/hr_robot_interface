a8283f8b6a5f861a8a8bba2206ec8cbf51b0fbd4
Nikita Dorodnykh <tualatin32@mail.ru>
2020-03-17 17:58:36 +0800

    Delete outdated controller and add forehead feature view

---
+++ modules/main/views/analysis-result/_forehead_features.php
@@ -0,0 +1,13 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="well">
+            <?= Html::a('Таблица признаков',
+                'http://84.201.129.65:9999/Preprocessor/Main.php?LengthOfTrend=4&TotalValue=25') ?>
+        </div>
+    </div>
+</div>
