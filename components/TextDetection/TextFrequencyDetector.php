e83bc072a82a335fbc692c2870816d28f1fa0ea2
LedZeppe1in <tualatin32@mail.ru>
2025-10-27 07:39:11 +0800

    Refactors

--- components/TextDetection/TextFrequencyDetector.php
+++ components/TextDetection/TextFrequencyDetector.php
@@ -9,21 +9,19 @@ class TextFrequencyDetector
     //-1 - текста в указанный момент времени нет
     //0,... - текст в указнное время есть и возварщается индекс элемента в массиве распознанных слов
 
-    public static function IsTextInTime($theTime, $theTextData,$theStartIndex)
+    public static function IsTextInTime($theTime, $theTextData, $theStartIndex)
     {
-        if (isset($theTime) && isset($theTextData) && is_array($theTextData)  && $theTime>0)
-        {
-            $N=count($theTextData);
-            $startFrom=0;
-            if (isset($theStartIndex) && $theStartIndex>=0) $startFrom=$theStartIndex;
-
-            for ($i=$startFrom; $i<$N; $i++)
-            {
-                if (isset($theTextData[$i][1]) && isset($theTextData[$i][2]))
-                {
-                    $startTime=$theTextData[$i][1];
-                    $durationTime=$theTextData[$i][2];
-                    if ($theTime>$startTime && $theTime<$startTime+$durationTime)return $i;
+        if (isset($theTime) && isset($theTextData) && is_array($theTextData) && $theTime > 0) {
+            $N = count($theTextData);
+            $startFrom = 0;
+            if (isset($theStartIndex) && $theStartIndex >= 0)
+                $startFrom = $theStartIndex;
+            for ($i = $startFrom; $i < $N; $i++) {
+                if (isset($theTextData[$i][1]) && isset($theTextData[$i][2])) {
+                    $startTime = $theTextData[$i][1];
+                    $durationTime = $theTextData[$i][2];
+                    if ($theTime > $startTime && $theTime < $startTime + $durationTime)
+                        return $i;
                 }
             }
             return -1;
@@ -31,79 +29,44 @@ class TextFrequencyDetector
         return null;
     }
 
-
-
-    public static function CountSpeechFrequencyByWords($theTextData,$AnswerTime,$theStartIndex)
+    public static function CountSpeechFrequencyByWords($theTextData, $AnswerTime, $theStartIndex)
     {
-
-        if (isset($theTextData) && is_array($theTextData) && isset($AnswerTime))
-        {
-
-            $WordsCount=count($theTextData);
+        if (isset($theTextData) && is_array($theTextData) && isset($AnswerTime)) {
+            $WordsCount = count($theTextData);
             if (isset($theStartIndex))
-            {
-
-                if ($theStartIndex>=0)
-                {
-                    $WordsCount-=$theStartIndex;
-                }
-                else{
+                if ($theStartIndex >= 0)
+                    $WordsCount -= $theStartIndex;
+                else
                     return null;
-                }
-
-            }
-
-
-            if ($WordsCount>0 && $AnswerTime>0)
-            {
-                return (double)$WordsCount/$AnswerTime;
-            }
-
-
-        }
-
-
-        return null;
-
-    }
-
-    public static function ResponseStartTimeByWords($theTextData,$theOtherVoiceEndsAtSec)
-    {
-        if (isset($theTextData) && is_array($theTextData) && isset($theOtherVoiceEndsAtSec))
-        {
-
-            $WordCount=count($theTextData);
-            if ($WordCount>0 )
-            {
-                for ($i=0;$i<$WordCount;$i++)
-                {
-                    if ( isset($theTextData[$i]) && isset($theTextData[$i][1]))
-                    {
-                        if ($theTextData[$i][1]>$theOtherVoiceEndsAtSec) return $theTextData[$i][1];
-                    }
-                }
-            }
+            if ($WordsCount > 0 && $AnswerTime > 0)
+                return (double)$WordsCount / $AnswerTime;
         }
         return null;
     }
 
-    public static function ResponseStartIndexInWords($theTextData,$theOtherVoiceEndsAtSec)
+    public static function ResponseStartTimeByWords($theTextData, $theOtherVoiceEndsAtSec)
     {
-        if (isset($theTextData) && is_array($theTextData) && isset($theOtherVoiceEndsAtSec))
-        {
+        if (isset($theTextData) && is_array($theTextData) && isset($theOtherVoiceEndsAtSec)) {
+            $WordCount = count($theTextData);
+            if ($WordCount > 0)
+                for ($i = 0; $i < $WordCount; $i++)
+                    if (isset($theTextData[$i]) && isset($theTextData[$i][1]))
+                        if ($theTextData[$i][1] > $theOtherVoiceEndsAtSec)
+                            return $theTextData[$i][1];
+        }
+        return null;
+    }
 
-            $WordCount=count($theTextData);
-            if ($WordCount>0 )
-            {
-                for ($i=0;$i<$WordCount;$i++)
-                {
-                    if ( isset($theTextData[$i]) && isset($theTextData[$i][1]))
-                    {
-                        if ($theTextData[$i][1]>$theOtherVoiceEndsAtSec) return $i;
-                    }
-                }
+    public static function ResponseStartIndexInWords($theTextData, $theOtherVoiceEndsAtSec)
+    {
+        if (isset($theTextData) && is_array($theTextData) && isset($theOtherVoiceEndsAtSec)) {
+            $WordCount = count($theTextData);
+            if ($WordCount > 0)
+                for ($i = 0; $i < $WordCount; $i++)
+                    if (isset($theTextData[$i]) && isset($theTextData[$i][1]))
+                        if ($theTextData[$i][1]>$theOtherVoiceEndsAtSec)
+                            return $i;
                 return -1;
-            }
         }
         return null;
     }
